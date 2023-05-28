fed606923107f3141db80c1e7e148cca1589575e
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2023-05-28 17:08:43 -0300

    Stuff
    is this what people call code?

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# mcskingrabber
- simple minecraft skin grabber with JS (and cape)
+# MCSkinGrabber
+
+## Test this repo [here](https://lukiiy.github.io/mcskingrabber/)
+
+A simple minecraft (java edition) skin grabber with JS (and cape) using Electroid's Mojang API.  
+**The API can be found [here](https://github.com/Electroid/mojang-api)!**  
+  
+Also sorry for the bad code, I was in a hurry while coding it for my website.
---
+++ app.js
@@ -0,0 +1,90 @@
+const loadingDiv = document.getElementById("loading");
+const capeDiv = document.getElementById("cape");
+const skinDiv = document.getElementById("skin");
+const button = document.getElementById("button");
+
+async function getSkin() {
+    const username = document.getElementById("username").value.trim();
+
+    if (username === "") {
+        alert("Please enter a username.");
+        return;
+    }
+
+    capeDiv.innerHTML = "";
+    skinDiv.innerHTML = "";
+
+    toggleState("loading");
+
+    try {
+        const response = await fetch(`https://api.ashcon.app/mojang/v2/user/${username}`);
+        if (!response.ok) {throw new Error("Failed to retrieve player data!");}
+
+        const data = await response.json();
+
+        if (!data.textures || !data.textures.skin) {
+            throw new Error("Failed to retrieve skin data!");
+        }
+
+        const skinUrl = data.textures.skin.url?.replace(/^http:\/\//i, "https://");
+        const capeUrl = data.textures.cape?.url?.replace(/^http:\/\//i, "https://");
+
+        await loadImage(skinUrl, "Skin", skinDiv);
+        if (capeUrl) {
+            await loadImage(capeUrl, "Cape", capeDiv);
+        }
+
+        toggleState("button");
+    } catch (error) {
+        toggleState("button");
+        alert(error.message);
+    }
+}
+
+async function loadImage(url, alt, div) {
+    if (url) {
+        const img = document.createElement("img");
+        img.src = url;
+        img.alt = alt;
+        await new Promise((resolve, reject) => {
+            img.onload = resolve;
+            img.onerror = reject;
+        });
+        div.appendChild(img);
+    }
+}
+
+function toggleState(state) {
+    const loadingDiv = document.getElementById("loading");
+    const button = document.getElementById("button");
+    loadingDiv.style.display = state === "loading" ? "block" : "none";
+    button.style.display = state === "button" ? "flex" : "none";
+}
+
+function dlContent(type) {
+    const imgContainer = type === "skin" ? document.querySelector("#skin") : document.querySelector("#cape");
+    const img = imgContainer.querySelector("img");
+    if (!img) {
+        alert("No image found!");
+        return;
+    }
+    fetch(img.src)
+        .then(response => response.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = type === "skin" ? "skin.png" : "cape.png";
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+        })
+        .catch(() => alert(`Error downloading ${type}!`));
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        const username = document.getElementById("username").value.trim();
+        if (username !== "") {getSkin();}
+    }
+});
